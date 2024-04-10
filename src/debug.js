77f467c4163f87413628fae88bd338ee3118699d
VeshonaS <aniahshell1@gmail.com>
2024-04-09 21:56:58 -0400

    finished sort of

--- src/debug.js
+++ src/debug.js
@@ -1,20 +1,22 @@
 const brokenLoop = (start, end) => {
   let result = '';
-  for (let i = start; i < end; i += 2) {
+  for (let i = start; i <= end; i ++) {
     result += i;
   }
   return result;
 };
+console.log(brokenLoop())
 
 const brokenNested = () => {
   let result = '';
   for (let i = 0; i < 2; i++) {
-    for (let i = 0; i < 10; i++) {
-      result += `-${i}${i}`;
+    for (let j = 0; j < 10; j++) {
+      result += `-${i}${j}`;
     }
   }
   return result.slice(1);
 };
+// console.log(brokenNested())
 
 module.exports = {
   brokenLoop,
--- src/modify.js
+++ src/modify.js
@@ -1,19 +1,20 @@
 const whileToFor = () => {
-  let i = 0;
-  while (i < 5) {
+  for(let i = 0; i < 5; i++) {
     console.log(i);
-    i++;
   }
 };
+// whileToFor()
 
 const continueGuardClause = () => {
   for (let i = 1; i < 5; i++) {
-    if (!(i === 2) && !(i === 3)) {
+    if (i === 2 || i ===3) {
       console.log("Sure glad this isn't 2 or 3");
-      console.log(i);
+      continue; 
     }
+      console.log(i);  
   }
 };
+continueGuardClause()
 
 module.exports = {
   whileToFor,
